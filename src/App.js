b0e9eea26aabb3d1a3d0c1081ca17e01bb772ff5
Varun Jain <varunjain972001@gmail.com>
2022-10-17 13:37:49 +0530

    Home and Blog Page Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Home from './Pages/Home';
+import Blog from './Pages/Blog';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
 
 class App extends React.Component
 {
   render()
   {
     return(<>
-    <h1>This is a blog project</h1>
+    <h1>This is a blog project By Varun Jain</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
     </>)
   }
 }
